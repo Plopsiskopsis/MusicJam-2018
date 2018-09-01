b93297b39b1833567ab4b6aeb0d3acbf5beb97a0
Plopsis <kirvesmies70@hotmail.com>
2018-09-01 17:12:48 +0300

    cool engine particle

--- scenes/mainship.gd
+++ scenes/mainship.gd
@@ -1,13 +1,20 @@
 extends Area
 
 var speed = 0.5
+var movement = Vector3()
 
 func _physics_process(delta):
+	movement = Vector3()
 	if Input.is_action_pressed("ui_left") && !Input.is_action_pressed("ui_right"):
-		translation.x += speed
+		movement.x += speed
 	if Input.is_action_pressed("ui_right") && !Input.is_action_pressed("ui_left"):
-		translation.x -= speed
+		movement.x -= speed
 	if Input.is_action_pressed("ui_up"):
-		translation.z += speed
+		movement.z += speed
 	if Input.is_action_pressed("ui_down"):
-		translation.z -= speed/2
+		movement.z -= speed/2
+	if movement != Vector3(0, 0, 0):
+		$engineparticles.emitting = true
+	else:
+		$engineparticles.emitting = false
+	translation += movement 
